f450635611c58eb5f15dbf86b6ebcf603a104bc4
Mihaela <mihaela.fedor@gmail.com>
2015-05-19 00:27:19 +0200

    Chief - getCoursesForTeacher; getTopCourses

--- [UI] Software Engeneering/app/chiefOfDepartment/chiefOfDepartment.js
+++ [UI] Software Engeneering/app/chiefOfDepartment/chiefOfDepartment.js
@@ -3,7 +3,8 @@
 .controller('ChiefCtrl', function ($scope, chiefFactory) {
 
     $scope.courses = [];
-    $scope.name = "";
+    $scope.nume = "Nume";
+    $scope.cadru = "Cadru"
 
     $scope.optionalSelected = false;
 
@@ -11,26 +12,55 @@
         $scope.optionalSelected = !$scope.optionalSelected;
     }
 
-    $scope.promise = chiefFactory.getProfessors().list;
 
-    if ($scope.promise != null) {
-        $scope.teachers = $scope.promise;
-        $scope.courses = $scope.promise[0].courses;
-        $scope.name = $scope.promise[0].name;
-    }
 
-    $scope.changeTeacher = function (teacher) {
-        if (teacher) {
+    chiefFactory.getProfessors().then(function (professors) {
+
+        $scope.teachers = professors;
+    });
+
+    chiefFactory.getEnrolled().then(function (enrolled) {
+
+        $scope.enrolled = enrolled;
+    });
+
+    chiefFactory.getCourses().then(function (courses) {
+
+        $scope.courses = courses;
+        angular.forEach($scope.courses, function (value, key) {
+            var sum = 0;
+            var nr = 0;
+
+            angular.forEach($scope.enrolled, function (enr, key2) {
+                if (enr.course == value.id) {
+                    sum += enr.grade;
+                    nr++;
+                }
+            });
+            if(nr > 0)
+                value.average = sum / nr;
+            else
+                value.average = 0;
+
+
+        });
+
+        console.log($scope.courses);
+    });
+
+
+
+    $scope.changeTeacher = function (teacherId) {
+        if (teacherId) {
             var teachers = $scope.teachers;
 
-            angular.forEach($scope.teachers, function (value , key) {
-                if (value.Id != null && value.Id == teacher) {
-                    $scope.courses = value.courses;
-                    $scope.name = value.name;
-      
+            angular.forEach($scope.teachers, function (value, key) {
+                if (value.id != null && value.id == teacherId) {
+                    $scope.coursesByTeacher = value.courses;
+
                 }
             })
         }
     }
-    
+
 })
