5cb55368d15e901e463ae170154723c8c9934423
unknown <fratean.tudor@gmail.com>
2015-05-19 01:44:52 -0700

    Worked on administrative staff

--- [UI] Software Engeneering/app/administrativeStaff/factory/adminStaffFactory.js
+++ [UI] Software Engeneering/app/administrativeStaff/factory/adminStaffFactory.js
@@ -1,71 +1,13 @@
 ﻿adminStaffModule
 .factory('adminStaffFactory', function (Restangular) {
     return {
+        getStudent: function () {
+            return Restangular.one('faculty').customGET('student/list/', {});
+        },
         getCourses: function () {
-            return Restangular.one('faculty').customGET('course/list', {});
+            return Restangular.one('faculty').customGET('course/list/', {});
         },
 
-
-       /* getCourses: function () {
-            return {
-                list:
-                [
-                   {
-                       Id: "AOP",
-                       Studenti: [
-                           {
-                               Id: "1",
-                               Group: "212",
-                               Year: 1,
-                               Name: "Petru",
-                               Grade: 8,
-                           },
-                           {
-                               Id: "2",
-                               Group: "911",
-                               Year: 2,
-                               Name: "Marcel",
-                               Grade: 5,
-                           },
-                           {
-                               Id: "3",
-                               Group: "915",
-                               Year: 2,
-                               Name: "Andrea",
-                               Grade: 10,
-                           }
-                       ]
-                   },
-
-                    {
-                    Id: "OS",
-                    Studenti: [
-                        {
-                            Id: "4",
-                            Group: "225",
-                            Year: 1,
-                            Name: "Raluca",
-                            Grade: 7,
-                        },
-                        {
-                            Id: "5",
-                            Group: "923",
-                            Year: 2,
-                            Name: "Victor",
-                            Grade: 8,
-                        },
-                        {
-                            Id: "6",
-                            Group: "916",
-                            Year: 1,
-                            Name: "Pavel",
-                            Grade: 10,
-                        }
-                    ]
-                }
-                ]
-            }
-        }*/
     }
 
 })
